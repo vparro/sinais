f7070aadbbea9c7c348f5cf42e6b2cb77f196a83
Vanderlei Parro <vparro@ieee.org>
2021-02-25 13:02:45 -0300

    Aula de ETE204
    Criação do código para avaliação de uma onada quadrada

---
+++ 2 - Fourier/1 - Serie/ECM307_Aula01.m
@@ -0,0 +1,1 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 0 - Boas práticas clear all;          % limpa as variáveisclose all;          % fecha todas as figurasclc;                % limpa a telapkg load symbolic;  % carrega o pacote simbólico - Octave
